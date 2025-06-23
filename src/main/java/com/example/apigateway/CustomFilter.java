5a6aeef743a84ce8eadaf6f141945d5ddfc2b8b3
audreydatau <audreydatau@gmail.com>
2025-06-23 15:24:58 +0700

    API Gateway Custom Filter

--- src/main/java/com/example/apigateway/CustomFilter.java
+++ src/main/java/com/example/apigateway/CustomFilter.java
@@ -1,23 +1,8 @@
 package com.example.apigateway;
 
-
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-//@Component
-//public abstract class CustomFilter implements GatewayFilter {
-//
-//    @Override
-//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-//        System.out.println("Before request processing");
-//        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-//            System.out.println("After response processing");
-//        }));
-//    }
-//}
 
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 
@@ -36,7 +21,6 @@ public class CustomFilter extends AbstractGatewayFilterFactory<CustomFilter.Conf
     public GatewayFilter apply(Config config) {
         return (exchange, chain) -> {
             String path = exchange.getRequest().getURI().getPath();
-            String x = exchange.getRequest().getHeaders()
             logger.info(config.getBeforeMessage() + path);
             return chain.filter(exchange)
                     .then(Mono.fromRunnable(() -> logger.info(config.getAfterMessage() + path)));
